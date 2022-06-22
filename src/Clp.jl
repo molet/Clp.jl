2b97b746704606059087d948ebd6dc1ea85de363
Letif Mones <letif.mones@invenialabs.co.uk>
2022-06-22 11:23:35 +0100

    remove version check

--- src/Clp.jl
+++ src/Clp.jl
@@ -11,14 +11,14 @@ function __init__()
     global libClp = Clp_jll.libClp
     version =
         VersionNumber("$(Clp_VersionMajor()).$(Clp_VersionMinor()).$(Clp_VersionRelease())")
-    if !(v"1.17.2" <= version <= v"1.17.6")
-        error(
-            "You have installed version $version of Clp, which is not " *
-            "supported by Clp.jl. If the version change was breaking, changes " *
-            "will need to be made to the Julia code. Please open an issue at " *
-            "https://github.com/jump-dev/Clp.jl.",
-        )
-    end
+#   if !(v"1.17.2" <= version <= v"1.17.6")
+#       error(
+#           "You have installed version $version of Clp, which is not " *
+#           "supported by Clp.jl. If the version change was breaking, changes " *
+#           "will need to be made to the Julia code. Please open an issue at " *
+#           "https://github.com/jump-dev/Clp.jl.",
+#       )
+#   end
     return
 end
 
